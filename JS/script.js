99f033b71474a57c6a9a1a08be54cffece229ef2
FelipeSGoncalves <felipe.goncalves.foz@Hotmail.com>
2022-03-05 09:13:42 -0300

    atualizando

--- JS/script.js
+++ JS/script.js
@@ -5,6 +5,7 @@
     
     const CANVAS_WIDTH = canvas.width = 1400;
     const CANVAS_HEIGHT = canvas.height = 720;
+    let gameSpeed = 1;
     
     const backgroundImage = new Image();
     backgroundImage.src = '../IMG/jupiter/background.png';
@@ -23,10 +24,6 @@
 
     const boxImage = new Image();
     boxImage.src = '../IMG/wooden_box.png';
-
-
-
-    let gameSpeed = 5;
     class InputHandler{
         constructor(){
             this.keys = [];
@@ -91,79 +88,36 @@
     
     class Box{
         constructor(image){
-            this.width = 100;
-            this.height = 100;
+            this.width = 150;
+            this.height = 150;
             this.x = CANVAS_WIDTH/2;
-            this.y = 355;
+            this.y = 305;
             this.image = image;
         }   
         draw(){
             ctx.drawImage(this.image, this.x, this.y, this.width, this.height);
         } 
     }
-    // class Robot{
-    //     constructor(gameWidth, gameHeight){
-    //         this.gameWidth = gameWidth;
-    //         this.gameHeight = gameHeight;
-    //         this.width = 100;
-    //         this.height = 100;
-    //         this.x = 0;
-    //         this.y = gameHeight - 100;
-    //         this.image = document.getElementById('robotImage');
-    //         this.Framex = 0;
-    //         this.Framey= 0;
-    //         this.speed = 0;
-    //         this.vy =0;
-    //         this.weight = 1;
-    //         this.tempo = 0;
-    //     }
-    //     draw(context){
-    //         context.fillStyle = 'transparent';
-    //         context.fillRect(this.x, this.y, this.width, this. height);
-    //         context.drawImage(this.image, this.Framex * this.width, this.Framey * this.height,
-    //         this.width, this.height , this.x, this.y, this.width, this.height);
-    //     }
-    //     update(input){
-    //         if(input.keys.indexOf('ArrowRight') > -1){
-    //             this.speed = 5;
-    //             if(this.tempo <= 4){
-    //                 this.Framex++;
-    //             }else{
-    //                 this.Framex--;
-    //             }
-                
-    //         }else if(input.keys.indexOf('ArrowLeft') > -1){
-    //             this.speed = -5;
-    //         }else if(input.keys.indexOf('ArrowUp') > -1 && this.onGround()){
-    //             this.vy -= 15;
-    //         }else{
-    //             this.speed = 0;
-    //             this.Framex = 0;
-    //         }
-    //         this.x += this.speed;
-        
-    //         if(this.x < 0){
-    //             this.x=0;
-    //         }else if (this.x > this.gameWidth - this.width){
-    //             this.x = this.gameWidth - this.width;
-    //         }
-    //         this.y += this.vy;
-            
-    //         if(!this.onGround()){
-    //             this.vy += this.weight;
-    //             this.Framey = 2;
-    //         }else{
-    //             this.vy = 0;
-    //             this.Framey = 0;
-    //         }
-    //         if (this.y > this.gameHeight - this.height){
-    //             this.y = this.gameHeight - this.height;
-    //         }
-    //     }
-    //     onGround(){
-    //         return this.y >= this.gameHeight - this.height;
-    //     }
-    // }
+    
+    class Robot{
+        constructor(image){
+            this.width = 217;
+            this.height = 200;
+            this.x = CANVAS_WIDTH / 2 - 215;
+            this.y = CANVAS_HEIGHT - 450;
+            this.image = image;
+            this.Framex = 0;
+            this.Framey= 0;
+            this.speed = 0;
+            this.vy =0;
+            this.weight = 1;
+            this.tempo = 0;
+        }
+        draw(context){
+            ctx.fillStyle = 'transparent';
+            ctx.drawImage(this.image, this.x, this.y, this.width, this.height);
+        }
+    }
 
     const input = new InputHandler();
     
@@ -172,7 +126,8 @@
     const floor = new Background(floorImage, 1);
     const layer = new Layer(layerImage);
     const box = new Box(boxImage);
-    
+    const robot = new Robot(robotImage);
+
     const gameBackground = [background, tower, floor];
 
     function animate(){
@@ -183,14 +138,13 @@
             object.draw();
         });
         box.draw();
+        robot.draw();
         requestAnimationFrame(animate);
     }   
     animate();
 
 /**let gameSpeed =  0;
     
-
-  
     function calculandoAtrito(miEstatico, massa, gravidade){
         forcaDeAtritoMax = miEstatico * massa * gravidade;
       
@@ -212,80 +166,6 @@ function animate(){
     
     requestAnimationFrame(animate);
 };
-
-function KeyDown(evt){
-    gameSpeed = 0;
-    switch (evt.keyCode) {
-        case 37: //esquerda
-            gameSpeed = 0;
-            break;
-        case 39:  //direita
-            gameSpeed++;
-            animate();
-            break;
-
-        default:
-            break;
-    }
-}
-
-    window.addEventListener('keydown', KeyDown, true);
-*/
-/** 
-const POSICAO_HORIZONTAL = 0;
-const POSICAO_VERTICAL = 0;
-const POSICAO_HORIZONTAL_CAIXA = 0;
-
-let contadorDeFrames = 0;
-let gameFrame = 0;
-const staggerFrames = 9;
-
-var canvas;
-var ctx;
-var dx = 5;//variação horizontal(velocidade)
-var dy = 5;//variação vertical(velocidade)
-var x = 300;//posição horizontal do obj
-var y = 500;//posição vertical do obj
-
-var xc = 500;//posição horizontal do obj
-
-
-var w = 1000;//largura
-var h = 900;//altura
-
-function draw() {
-    ctx.beginPath();
-    ctx.fillStyle = "white";
-    ctx.rect(x, y, 50, 50)
-    ctx.rect(500, y, 50, 50)
-    ctx.fill();
-}
-
-function clear() {
-    ctx.fillStyle = "black";
-    ctx.strokeStyle = "white";
-    ctx.beginPath();
-    ctx.rect(0, 0, w, h);
-    ctx.closePath();
-    ctx.fill();
-    ctx.stroke();
-}
-
-function start() {
-    canvas = document.getElementById("canvas");
-    ctx = canvas.getContext("2d");
-}
-
-
-function Atualizar() {
-    clear();    
-    draw();
-}
-window.addEventListener('keydown', KeyDown, true);
-start();
-*/
-
-
     
 //     const LARGURA_SPRITE = 64;
 //     const ALTURA_SPRITE = 64;
@@ -302,3 +182,5 @@ start();
 //     gameFrame++;
 //     requestAnimationFrame(animate);
 // }
+
+*/
